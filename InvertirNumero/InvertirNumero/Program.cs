cab59d79fe25bfb7beee7fc82d123154521eab19
Sebastian Torres <camtorsoftware2@gmail.com>
2023-04-14 17:05:51 -0500

    cargue el primer ejercicio que e sinvertir numero de dos cifras

---
+++ InvertirNumero/InvertirNumero/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvertirNumero
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, div, residuo, numAxuiliar;
+            String linea;
+            Console.WriteLine("Ingrese el numero de dos cifras");
+            linea = Console.ReadLine();
+            num = Convert.ToInt32(linea);
+            div = num / 10;
+            residuo = num % 10;
+            numAxuiliar = (residuo * 10) + div;
+            Console.WriteLine("el numero invertido es {0}", numAxuiliar);
+            Console.ReadKey();
+        }
+    }
+}
